111a7331dd48cb098b9805566ef5353f49d90026
rence <99105241+rencejke@users.noreply.github.com>
2024-08-02 20:05:31 +0800

    updated projects page

--- src/components/pages/ProjectsPage.jsx
+++ src/components/pages/ProjectsPage.jsx
@@ -22,107 +22,221 @@ const ProjectsPage = () => {
       <h4 className='text-center font-thicker text-[2rem] mb-12'>Some of my <span className='text-[#00abf0]'>Projects</span></h4>
       <div className="wrapper ">
         
-        <div className='bg-[#00abf0] flex  rounded-lg p-2 justify-around items-center w-[500px] mx-auto'>
+        <div className='bg-[#00abf0] flex flex-col lg:flex-row lg:gap-0 gap-2 rounded-lg py-1 justify-center lg:justify-around items-center lg:w-[500px] mx-auto'>
           <button id="1" onClick={()=> handleClick(1)}
-          className={`${checkActive(1, "bg-[#081b29] px-3 py-1 rounded-md text-[#ededed] transition")}`}
+          className={`${checkActive(1, "bg-[#081b29] px-3 rounded-md text-[#ededed] transition")}`}
           >Web Development</button>
           <button id="2"  onClick={()=> handleClick(2)}
-          className={`${checkActive(2, "bg-[#081b29] px-3 py-1 rounded-md text-[#ededed] transition")}`}
+          className={`${checkActive(2, "bg-[#081b29] px-3 rounded-md text-[#ededed] transition")}`}
           >Mobile Development</button>
           <button id="3"  onClick={()=> handleClick(3)}
-           className={`${checkActive(3, "bg-[#081b29] px-3 py-1 rounded-md text-[#ededed] transition")}`}
+           className={`${checkActive(3, "bg-[#081b29] px-3 rounded-md text-[#ededed] transition")}`}
           >Others...</button>
         </div>
 
-        <div className='max-w-[1000px] w-[100%] mx-auto mt-12'>
-        <div id="1" className={`${checkActive(1, "active")} panel`}>
+        <div className='flex flex-col max-w-[1000px] w-[100%] mx-auto mt-12'>
+
+        {/* project1 */}
+       <div id="1" className={`${checkActive(1, "active")} panel`}>
          
-          <div className='tab-item flex flex-row gap-10 bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
-            <div className='left w-[100%]'>
-            <img src="https://via.placeholder.com/200x200" alt="" className='w-[300px] h-[230px] rounded-xl object-cover'/>
-            
-            </div>
+       <div className='tab-item flex flex-col gap-10 lg:flex-row  bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
+  <div className='left max-w-[320px]'>
+    <img src="https://via.placeholder.com/200x200" alt="" className='w-[450px] h-[230px] rounded-xl object-cover'/>
+  </div>
 
-            <div className='right text-[#081b29]'>
-            <h3 className='font-bold dark:text-[#ededed]'>Math-alino</h3>
-            <p className='dark:text-[#ededed]'>Lorem ipsum dolor sit
-             amet consectetur adipisicing elit. Ex asperiores
-              atque et sunt commodi veritatis, harum ratione quae nisi 
-              debitis non perspiciatis doloribus dicta perferendis, cum dolorum laborum quibusdam quia.</p>
-            <div className='mb-4 flex gap-2 items-center'>
-            <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
-              <ul className='flex gap-4'>
-                <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>C#</li>
-                <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>Unity</li>
-                <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29]  items-center'>Canva</li>
-              </ul>
-            </div>
-             <div className="btn-container w-[345px] h-[50px] ">
-             <Link><button>Learn More</button></Link>
+  <div className='right text-[#081b29] max-w-[600px]x'>
+   <div className='grid grid-rows-1 gap-10'>
+   <div>
+    <h3 className='font-bold dark:text-[#ededed]'>Techida Clone</h3>
+    <p className='dark:text-[#ededed] text-wrap text-[14px]'>
+      Cloned project: Techida is an IT Solutions & Business Services Responsive template that is the perfect choice for tech businesses.
+    </p>
+    <div className='mb-4 flex flex-wrap gap-2 items-center'>
+      <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
+      <ul className='flex flex-wrap gap-2 items-center'>
+        <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>HTML</li>
+        <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>CSS</li>
+        <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>JS</li>
+        <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>Tailwind</li>
+        <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>Wordpress</li>
+      </ul>
+    </div>
+    </div>
+    <div className="btn-container w-[345px] h-[50px] ">
+      <Link to={'https://github.com/lourenrubico23/Techida-Collab'}>
+        <button>Learn More</button>
+      </Link>
+    </div>
+   </div>
+  </div>
+</div>
+       </div>
+
+       <div id="1" className={`${checkActive(1, "active")} panel`}>  
+       <div className='tab-item flex flex-col gap-10 lg:flex-row   bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
+       <div className='left max-w-[320px]'>
+       <img src="https://via.placeholder.com/200x200" alt="" className='w-[450px] h-[230px] rounded-xl object-cover'/>
+      </div>
+
+           <div className='right text-[#081b29] max-w-[600px]'>
+           <div className='grid grid-rows-1'>
+           <div>
+           <h3 className='font-bold dark:text-[#ededed]'>FCA Clone Page</h3>
+           <p className='dark:text-[#ededed] text-wrap text-[14px]'>Frontline Christian Academy, Inc. (FCA) started as a homeschool for the children of staff members at Philippine Frontline Ministries, Inc. (PFM) (www.thefrontline.asia). PFM later expanded its mission to help abandoned and abused street children by creating a child care initiative called Face the Children (FTC) (www.facethechildren.org).</p>
+           <div className='mb-4 flex gap-2 items-center'>
+           <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
+             <ul className='flex flex-wrap gap-1 items-center lg:gap-[6px]'>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>HTML</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>CSS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>JS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29]  items-center'>Tailwind</li>
+             </ul>
              </div>
+           </div>
+            <div className="btn-container w-[345px] h-[50px] ">
+            <Link to={'https://github.com/rencejke/FAQS__Tailwind__Practice'}><button>Learn More</button></Link>
             </div>
-          </div>
+           </div>
+           </div>
+         </div>
+       </div>
 
-   
+       <div id="1" className={`${checkActive(1, "active")} panel`}>  
+       <div className='tab-item flex flex-col gap-10 lg:flex-row   bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
+       <div className='left max-w-[320px]'>
+       <img src="https://via.placeholder.com/200x200" alt="" className='w-[450px] h-[230px] rounded-xl object-cover'/>
+      </div>
 
-          
+           <div className='right text-[#081b29] max-w-[600px]'>
+           <div className='grid grid-rows-1 gap-10'>
+           <div>
+           <h3 className='font-bold dark:text-[#ededed]'>Frontline Business Solutions</h3>
+           <p className='dark:text-[#ededed] text-wrap text-[14px]'>
 
-          
+            Cloned project: Frontline Business Solutions 
+            is a company based in San Pablo City, Laguna, 
+            Philippines. Offers website design, web hosting, 
+            accounting, and SEO.
+
+           </p>
+           <div className='mb-4 flex gap-2 items-center'>
+           <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
+             <ul className='flex flex-wrap gap-1 items-center lg:gap-[6px]'>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>HTML</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>CSS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>JS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29]  items-center'>Tailwind</li>
+             </ul>
+             </div>
+           </div>
+            <div className="btn-container w-[345px] h-[50px] ">
+            <Link to={'https://github.com/rencejke/Frontline_Website'}><button>Learn More</button></Link>
+            </div>
+           </div>
+           </div>
+         </div>
+       </div>
+
+       <div id="1" className={`${checkActive(1, "active")} panel`}>  
+       <div className='tab-item flex flex-col gap-10 lg:flex-row  bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
+       <div className='left max-w-[320px]'>
+       <img src="https://via.placeholder.com/200x200" alt="" className='w-[450px] h-[230px] rounded-xl object-cover'/>
+      </div>
+
+           <div className='right text-[#081b29] max-w-[600px]'>
+           <div className='grid grid-rows-1 gap-10'>
+           <div>
+           <h3 className='font-bold dark:text-[#ededed]'>Insights Clone</h3>
+           <p className='dark:text-[#ededed] text-wrap text-[14px]'>
+
+           Insights Clone is a project that is given to us by our instructor month of May year 2024
+
+           </p>
+           <div className='mb-4 flex gap-2 items-center'>
+           <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
+             <ul className='flex flex-wrap gap-1 items-center lg:gap-[6px]'>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>HTML</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>CSS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>JS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29]  items-center'>Tailwind</li>
+             </ul>
+             </div>
+           </div>
+            <div className="btn-container w-[345px] h-[50px] ">
+            <Link to={'https://github.com/rencejke/Frontline_Website'}><button>Learn More</button></Link>
+            </div>
+           </div>
+           </div>
+         </div>
+       </div>
+
+       <div id="1" className={`${checkActive(1, "active")} panel`}>  
+       <div className='tab-item flex flex-col gap-10 lg:flex-row  bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
+       <div className='left max-w-[320px]'>
+       <img src="https://via.placeholder.com/200x200" alt="" className='w-[450px] h-[230px] rounded-xl object-cover'/>
+      </div>
+
+           <div className='right text-[#081b29] max-w-[600px]'>
+           <div className='grid grid-rows-1 gap-10'>
+           <div>
+           <h3 className='font-bold dark:text-[#ededed]'>Library Management System</h3>
+           <p className='dark:text-[#ededed] text-wrap text-[14px]'>
+
+           Library Management System that can handle everyday transactions of a library. Admin can view, update, add, delete books and students can borrow or return book
+
+           </p>
+           <div className='mb-4 flex gap-2 items-center'>
+           <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
+             <ul className='flex flex-wrap gap-1 items-center lg:gap-[6px]'>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>HTML</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>CSS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>JS</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>PHP</li>
+             </ul>
+             </div>
+           </div>
+            <div className="btn-container w-[345px] h-[50px] ">
+            <Link to={'https://github.com/rencejke/project-library-management'}><button>Learn More</button></Link>
+            </div>
+           </div>
+           </div>
+         </div>
+       </div>
+
        
 
-        </div>
-        <div id="2" className={`${checkActive(2, "active")} panel`}>
-          <div className='flex flex-row gap-10 bg-[#ededed] p-6 rounded-md'>
-            <div className='left w-[100%]'>
-            <img src="https://via.placeholder.com/200x200" alt="" className='w-[300px] h-[230px] rounded-xl object-cover'/>
-            
-            </div>
+       <div id="1" className={`${checkActive(1, "active")} panel`}>  
+       <div className='tab-item flex flex-col gap-10 lg:flex-row bg-[#ededed] dark:bg-[#0c1216] p-6 rounded-md mb-5'>
+       <div className='left max-w-[320px]'>
+       <img src="https://via.placeholder.com/200x200" alt="" className='w-[450px] h-[230px] rounded-xl object-cover'/>
+      </div>
 
-            <div className='right text-[#081b29]'>
-            <h3 className='font-bold '>Math-alino</h3>
-            <p className=''>Lorem ipsum dolor sit
-             amet consectetur adipisicing elit. Ex asperiores
-              atque et sunt commodi veritatis, harum ratione quae nisi 
-              debitis non perspiciatis doloribus dicta perferendis, cum dolorum laborum quibusdam quia.</p>
-            <div className='mb-4 flex gap-2 items-center'>
-            <h4 className='font-bold '>Technologies:</h4>
-              <ul className='flex gap-4'>
-                <li className='bg-[#081b29] px-2 py-1 rounded-md text-[#ededed] items-center'>C#</li>
-                <li className='bg-[#081b29] px-2 py-1 rounded-md text-[#ededed] items-center'>Unity</li>
-                <li className='bg-[#081b29] px-2 py-1 rounded-md text-[#ededed] items-center'>Canva</li>
-              </ul>
-            </div>
-             <div className="btn-container w-[345px] h-[50px] ">
-             <Link><button>Learn More</button></Link>
-             </div>
-            </div>
-          </div></div>
-        <div id="3" className={`${checkActive(3, "active")} panel`}>
-          <div className='flex flex-row gap-10 bg-[#ededed] p-6 rounded-md'>
-            <div className='left w-[100%]'>
-            <img src="https://via.placeholder.com/200x200" alt="" className='w-[300px] h-[230px] rounded-xl object-cover'/>
-            
-            </div>
+           <div className='right text-[#081b29] max-w-[600px]'>
+           <div className='grid grid-rows-1 gap-10'>
+           <div>
+           <h3 className='font-bold dark:text-[#ededed]'>Michi Ramen</h3>
+           <p className='dark:text-[#ededed] text-wrap text-[14px]'>
 
-            <div className='right text-[#081b29]'>
-            <h3 className='font-bold '>Math-alino</h3>
-            <p className=''>Lorem ipsum dolor sit
-             amet consectetur adipisicing elit. Ex asperiores
-              atque et sunt commodi veritatis, harum ratione quae nisi 
-              debitis non perspiciatis doloribus dicta perferendis, cum dolorum laborum quibusdam quia.</p>
-            <div className='mb-4 flex gap-2 items-center'>
-            <h4 className='font-bold '>Technologies:</h4>
-              <ul className='flex gap-4'>
-                <li className='bg-[#081b29] px-2 py-1 rounded-md text-[#ededed] items-center'>C#</li>
-                <li className='bg-[#081b29] px-2 py-1 rounded-md text-[#ededed] items-center'>Unity</li>
-                <li className='bg-[#081b29] px-2 py-1 rounded-md text-[#ededed] items-center'>Canva</li>
-              </ul>
-            </div>
-             <div className="btn-container w-[345px] h-[50px] ">
-             <Link><button>Learn More</button></Link>
+           Michi ramen is a cloned project
+
+           </p>
+           <div className='mb-4 flex gap-2 items-center'>
+           <h4 className='font-bold dark:text-[#ededed]'>Technologies:</h4>
+             <ul className='flex flex-wrap gap-1 items-center lg:gap-[6px]'>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>HTML</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>REACT</li>
+               <li className='bg-[#081b29] dark:bg-[#ededed] px-2 py-1 rounded-md text-[#ededed] dark:text-[#081b29] items-center'>TAILWIND</li>
+             </ul>
              </div>
+           </div>
+            <div className="btn-container w-[345px] h-[50px] ">
+            <Link to={'https://github.com/rencejke/project-library-management'}><button>Learn More</button></Link>
             </div>
-          </div></div>
+           </div>
+           </div>
+         </div>
+       </div>
+  
         </div>
 
       </div>
